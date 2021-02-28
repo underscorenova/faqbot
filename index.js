80940a8bd65ec94387cfba664c4d501eed5d1aa3
underscorenova <underscorenova@gmail.com>
2021-02-28 14:13:34 +0100

    joe

--- index.js
+++ index.js
@@ -63,3 +63,10 @@ client.on("message", async (message) => {
     }
   }
 });
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('ok');
+});
+server.listen(3000);
